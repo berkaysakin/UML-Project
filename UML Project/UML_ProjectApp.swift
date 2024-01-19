c86b7eb66669f040a951c09d4d71db0685263caa
berkaysakin <berkaysakin500@gmail.com>
2024-01-19 21:42:54 +0300

    Initial Commit

---
+++ UML Project/UML_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  UML_ProjectApp.swift
+//  UML Project
+//
+//  Created by berkay on 19.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct UML_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
