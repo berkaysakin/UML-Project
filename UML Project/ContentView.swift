c3d05e4b627268e5b5e7d18de3d9568d18435818
berkaysakin <berkaysakin500@gmail.com>
2024-01-19 21:54:31 +0300

    University Management added

--- UML Project/ContentView.swift
+++ UML Project/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is for UML")
         }
         .padding()
     }
